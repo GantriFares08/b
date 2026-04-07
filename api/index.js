bafb0daea50705d3b1c25bfa906084852e6ad5b5
GantriFares08 <128326611+GantriFares08@users.noreply.github.com>
2026-04-07 14:45:41 +0200

    tt

--- api/index.js
+++ api/index.js
@@ -6,5 +6,11 @@ app.get("/", (req, res) => {
   res.json({ message: "API is working 🚀" });
 });
 
-// IMPORTANT for Vercel
-export default app;
+app.get("/api", (req, res) => {
+  res.json({ message: "API is working 🚀" });
+});
+
+// Vercel needs this exact signature
+export default function handler(req, res) {
+  return app(req, res);
+}
