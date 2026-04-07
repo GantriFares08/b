ccabe4ae20988d0402f4d0964f07872e21ad0522
GantriFares08 <128326611+GantriFares08@users.noreply.github.com>
2026-04-07 13:30:41 +0200

    tt

--- api/index.js
+++ api/index.js
@@ -1,28 +1,10 @@
-import express from 'express';
-import "dotenv/config";
-import cors from 'cors';
-import { clerkMiddleware } from '@clerk/express'
-import { serve } from "inngest/express";
-import { inngest, functions } from "../inngest/index.js"
+import express from "express";
+
 const app = express();
 
-app.use(express.json());
-app.use(cors())
-app.use(clerkMiddleware())
-
 app.get("/", (req, res) => {
-    res.json({ 
-        message: "Server is live!",
-        status: "OK",
-        endpoints: {
-            health: "/",
-            inngest: "/api/inngest"
-        }
-    })
-})
+  res.json({ message: "API is working 🚀" });
+});
 
-
-app.use("/api/inngest", serve({ client: inngest, functions }));
-
-
+// IMPORTANT for Vercel
 export default app;
