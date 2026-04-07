845dee863bc26b95fe7717b83e669b823ec3eb26
GantriFares08 <128326611+GantriFares08@users.noreply.github.com>
2026-04-07 15:06:49 +0200

    pin prisma version

--- api/index.js
+++ api/index.js
@@ -1,3 +1,28 @@
-export default function handler(req, res) {
-  res.status(200).json({ message: "hello" });
-}
+import express from 'express';
+import "dotenv/config";
+import cors from 'cors';
+//import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "../inngest/index.js"
+const app = express();
+
+app.use(express.json());
+app.use(cors())
+app.use(clerkMiddleware())
+
+app.get("/", (req, res) => {
+    res.json({ 
+        message: "Server is live!",
+        status: "OK",
+        endpoints: {
+            health: "/",
+            inngest: "/api/inngest"
+        }
+    })
+})
+
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
+
+export default app;
