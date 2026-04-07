1474a5f711c87e8e6260427a06c4cf6f285be2ec
GantriFares08 <128326611+GantriFares08@users.noreply.github.com>
2026-04-07 14:47:38 +0200

    tt

--- api/index.js
+++ api/index.js
@@ -1,16 +1,3 @@
-import express from "express";
-
-const app = express();
-
-app.get("/", (req, res) => {
-  res.json({ message: "API is working 🚀" });
-});
-
-app.get("/api", (req, res) => {
-  res.json({ message: "API is working 🚀" });
-});
-
-// Vercel needs this exact signature
 export default function handler(req, res) {
-  return app(req, res);
+  res.status(200).json({ message: "hello" });
 }
