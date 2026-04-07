7fd745340cad794ee355f255e0f1b61a2db539e4
GantriFares08 <128326611+GantriFares08@users.noreply.github.com>
2026-04-07 15:53:27 +0200

    pin prisma version

--- inngest/index.js
+++ inngest/index.js
@@ -18,7 +18,7 @@ const SyncUserCreation = inngest.createFunction(
         await prisma.user.update({
             where:{id:data.id},
             data:{
-                email: data?.email_addresses[0]?.email_addresses,
+                email: data?.email_addresses[0]?.email_address,
                 name:data?.first_name+" "+data?.last_name,
                 image:data?.image_url,
 
